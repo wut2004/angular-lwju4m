1f0405632ce117d0d38bccb4620c02ada1e6453c
wut2004 <tomwuts@gmail.com>
2020-04-21 08:55:06 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AskComponent } from './ask/ask.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, AskComponent, SearchComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
